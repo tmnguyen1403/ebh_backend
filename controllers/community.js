a3285fdbc23f7eee2f05855a680b17eb084a18b4
Minh Nguyen <tmnguyen1403@gmail.com>
2020-08-02 02:26:27 -0500

    Add method to create community

---
+++ controllers/community.js
@@ -0,0 +1,24 @@
+const Community = require("../models/community")
+
+exports.getCreateCommunityView = (req, res) => {
+	res.render("createCommunity")
+}
+
+exports.createCommunity = (req, res) => {
+	const data = {
+		name: req.body.name,
+		coordinators: [],
+	}
+	Community.create(data)
+		.then(record => {
+			console.log("Create community: ", record)
+			res.render("confirm", {
+				type: "community",
+				name: data.name}
+			)
+		})
+		.catch(error => {
+			console.log("Error when create community", error)
+			res.send("Error when create community")
+		})
+}
