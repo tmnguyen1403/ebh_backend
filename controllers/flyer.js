8f8e72012d33b97e47b4d46f3499ef9fd436139b
Minh Nguyen <tmnguyen1403@gmail.com>
2020-08-10 01:17:18 -0500

    Add code to remove Old files when update flyer

--- controllers/flyer.js
+++ controllers/flyer.js
@@ -1,6 +1,8 @@
 const Flyer = require("../models/flyer")
 const formidable = require("formidable")
 const util = require('util')
+const fs = require('fs')
+const UPLOAD_PATH = process.cwd() + '/public/uploads'
 /*------------------utils function--------------------*/
 const getFileName = (file) => {
 	if (file && file.path) {
@@ -9,26 +11,56 @@ const getFileName = (file) => {
 	}
 	return null
 }
+const deleteFile = (path) => {
+	fs.unlink(path, (error) => {
+		if (error) throw error
+		console.log(`${path} was deleted`)
+	})
+}
+
+const isDefine = (a) => {
+	return (a && a!== undefined && a.length > 0)
+}
+
+/*------------------end utils function--------------------*/
 
 const getFlyerParams = async (req) => {
 	console.log("getFlyerParams")
 	const form = new formidable.IncomingForm();
-	form.uploadDir = process.cwd() + '/public/uploads'
+	form.uploadDir = UPLOAD_PATH
 	form.keepExtensions = true
 	const parsePromise = (req) => {
 		return new Promise((resolve, reject) => {
 			form.parse(req, async (error, fields, files) => {
 				if (error)
 					return reject(error)
+				//console.log("ParsePromise ", fields.deleteFiles)
+				/*Remove Old Data*/
+				let dFiles = fields.deleteFiles
+				if (isDefine(dFiles)) {
+					dFiles = dFiles.split(";")
+						.filter(value => value.length > 0)
+					let fnames = dFiles.map(file => file.substr(file.lastIndexOf("/")+ 1))
+					let fpaths = fnames.map(name =>
+						UPLOAD_PATH + "/"+name)
+					try {
+						fpaths.forEach(path => deleteFile(path))
+						console.log("Complete Remove Old Data")
+					} catch(error) {
+						console.log(`Error deleting file `, error.message)
+					}
+				}
+				/*End Remove Old Data*/
 				const { title, communityid } = fields
 				const background = getFileName(files.background)
 				const flyer1 = getFileName(files.flyer1)
 				const flyer2 = getFileName(files.flyer2)
-				console.log(background)
-				if (background === null)
-					return reject (new
-						Error("Please provide background image for flyer"))
-				let params = {title, background}
+
+				let params = {}
+				if (background)
+					params['background'] = background
+				if (title)
+					params['title'] = title
 				if (flyer1)
 					params['flyer1'] = flyer1
 				if (flyer2)
@@ -117,7 +149,9 @@ const create = async (req, res, next) => {
 	try {
 		const flyerParams = await getFlyerParams(req)
 		console.log("FlyerPrams ", flyerParams)
-
+		if (flyerParams["background"] === null)
+			return reject (new
+				Error("Please provide background image for flyer"))
 		const record = await Flyer.create(flyerParams)
 		console.log("Creating flyer successfully. Record", record)
 		res.json({
